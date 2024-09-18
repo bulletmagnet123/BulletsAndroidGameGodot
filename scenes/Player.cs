2d1301c0f583315261769cf2d3095bb9515a9964
bulletmagnet <bulletmagnet.123@gmail.com>
2024-09-17 22:00:19 -0700

    added player touchscreen controlls and finally gpt them working then rewrote the way it handles the input in the player class

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -1,40 +1,109 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 130.0f;
-	public const float JumpVelocity = -300.0f;
+
+
+
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	const float Speed = 130.0f;
+	const float JumpVelocity = -300.0f;
+	Vector2 velocity;
+
+
+
+	public override void _Ready()
+	{
+	}
+
+
+	public void moveRight()
+	{
+		velocity.X += Speed;
+		GD.Print("Moving RIGHT");
+	}
+
+	public void moveLeft()
+	{
+		velocity.X -= Speed;
+		GD.Print("Moving LEFT");
+	}
+
+
+	public void jump()
+	{
+		if (IsOnFloor())
+		{
+			this.velocity.Y = JumpVelocity;
+		}
+	}
+
+
+
+	public void attack()
+	{
+
+
+
+	}
+
+	void CheckHorizontalInput(double delta)
+	{
+		Vector2 velocity = Velocity;
+		float HorizontalMovement = Input.GetAxis("left", "right");
+		if (HorizontalMovement != 0)
+		{
+			velocity.X = HorizontalMovement * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+
+	}
+
+	void CheckVerticalInput(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		if (!IsOnFloor())
+		{
+			velocity.Y += gravity * (float)delta;
+		}
+
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+		float horizontalMovement = Input.GetAxis("left", "right");
+		if (horizontalMovement != 0)
+		{
+			velocity.X = horizontalMovement * Speed;
+		}
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
+		
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
+		CheckHorizontalInput(delta);
+		CheckVerticalInput(delta);
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-
+		// Gradually stop the player when no input is detected
+		velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed * (float)delta);
 		Velocity = velocity;
 		MoveAndSlide();
+
 	}
 }
+
+
---
+++ scripts/TouchControls.cs
@@ -0,0 +1,11 @@
+using System.Runtime.Serialization.Formatters;
+using Godot;
+public partial class TouchControls : CanvasLayer
+{
+
+    public Player player = new Player();
+
+
+
+
+}
